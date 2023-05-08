09b88c44ddb8a3971191be31d904e2c9e2d75f84
Kayo-b <kyo.bhz@gmail.com>
2023-05-08 22:31:23 +0200

    revision

--- easy/12-FirstBadVersion.js
+++ easy/12-FirstBadVersion.js
@@ -109,3 +109,17 @@ var solution = function(isBadVersion) {
         }
     }
 }
+
+var solution = (isBadVersion) => {
+    return function(n) {
+        let left = 1;
+        let right = n;
+        while(right > left) {
+            let mid = Math.floor(left + (right - left) / 2);
+            if(isBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+    
+}
